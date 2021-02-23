ba49a28a8d731167b575fb84c634b28b22568137
lahuer <lahuer99@gmail.com>
2021-02-23 17:16:01 +0530

    defense line added with for loop

--- index.py
+++ index.py
@@ -1,35 +1,51 @@
-from __future__ import print_function
-
 from PIL import Image,ImageDraw,ImageFont
  
 
 field=Image.open("./images/turf/field.jpg")
-border=Image.open("./images/turf/border.png")
+# border=Image.open("./images/turf/border.png")
 
 
 fieldn=field
 
 # messi=Image.open("messi.jpg")
 
-d1=ImageDraw.Draw(border)
-dfont=ImageFont.truetype("Roboto-Black.ttf",250)
 
-d1.text((100,110),"12",font=dfont,fill=(255,255,255))
 
-bordern=border.resize((450,450))
-# bordern.save("nos.png")
+
+#get input jersey nos from file
+inp=open("input.txt","r")
+txt=list(map(int,inp.read().split()))
+
+
+
+#make for loop for creating and putting bordered jersnos
 h=fieldn.size[1]-1500.0
+w=fieldn.size[0]
+print(w)
+wforeach=int(w/4)-150
+j=0
+for jno in txt:
+	print(jno)
+	border=Image.open("./images/turf/border.png")
+	d1=ImageDraw.Draw(border)
+	dfont=ImageFont.truetype("Roboto-Black.ttf",250)
+	d1.text((160,110),str(jno),font=dfont,fill=(255,255,255))
+	bordern=border.resize((450,450))
+	fieldn.paste(bordern,(580+(j*wforeach),int(h)))
+	j+=1
+	fieldn.save("how.jpg")
 
-fieldn.paste(bordern,(580,int(h)))
-fieldn.save("how.jpg")
-# messi1=messi
-# messi1=messi1.resize((340,340))
+	
 
-# print(border.format,border.size,border.mode)
-# print(field.format,field.size,field.mode)
-# print(messi.format,messi.size,messi.mode)
-# print(messi1.format,messi1.size,messi1.mode)
+# {
+# d1=ImageDraw.Draw(border)
+# dfont=ImageFont.truetype("Roboto-Black.ttf",250)
 
-# border.paste(messi1,(80,70));
+# d1.text((100,110),"12",font=dfont,fill=(255,255,255))
 
-# border.save("pasted.png")
+# bordern=border.resize((450,450))
+# h=fieldn.size[1]-1500.0
+
+# fieldn.paste(bordern,(580,int(h)))
+# fieldn.save("how.jpg")
+# }
