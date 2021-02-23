cfa612a09e16d7282bc14d29a384b92a858b8569
lahuer <lahuer99@gmail.com>
2021-02-23 18:07:08 +0530

    added all jersnos,fix spacing

--- index.py
+++ index.py
@@ -7,37 +7,81 @@ field=Image.open("./images/turf/field.jpg")
 
 fieldn=field
 
-# messi=Image.open("messi.jpg")
-
-
-
 
 #get input jersey nos from file
 inp=open("input.txt","r")
-txt=list(map(int,inp.read().split()))
+formation=[]
+for line in inp.readlines():
+	txt=list(map(int,line.split()))
+	formation.append(txt)
+
+# sections=len(formation)
+# print(sections)
+
+for m in formation:
+	print(m)
+
+
+hforeach=(fieldn.size[1]-300)/len(formation)
+
+hnos=0
+for line in formation:
+	wforeach=(fieldn.size[0]-200)/len(line)
+	wnos=1
+	for jersno in line:
+		border=Image.open("./images/turf/border.png")
+		d1=ImageDraw.Draw(border)
+		dfont=ImageFont.truetype("Roboto-Black.ttf",250)
+		d1.text((160,110),str(jersno),font=dfont,fill=(255,255,255))
+		bordern=border.resize((450,450))
+		fieldn.paste(bordern,(int(450+(wnos*wforeach)/2),int(700+(hnos*hforeach))))
+		fieldn.save("how.jpg")
+		wnos+=1
+	hnos+=1
 
 
 
-#make for loop for creating and putting bordered jersnos
-h=fieldn.size[1]-1500.0
-w=fieldn.size[0]
-print(w)
-wforeach=int(w/4)-150
-j=0
-for jno in txt:
-	print(jno)
-	border=Image.open("./images/turf/border.png")
-	d1=ImageDraw.Draw(border)
-	dfont=ImageFont.truetype("Roboto-Black.ttf",250)
-	d1.text((160,110),str(jno),font=dfont,fill=(255,255,255))
-	bordern=border.resize((450,450))
-	fieldn.paste(bordern,(580+(j*wforeach),int(h)))
-	j+=1
-	fieldn.save("how.jpg")
+# ---------------------------------------
 
-	
+
 
 # {
+# #make for loop for creating and putting bordered jersnos
+# h=fieldn.size[1]-1500.0
+
+
+# w=fieldn.size[0]
+# wforeach=int(w/4)-150
+
+
+# j=0
+# for jno in txt:
+# 	print(jno)
+# 	border=Image.open("./images/turf/border.png")
+# 	d1=ImageDraw.Draw(border)
+# 	dfont=ImageFont.truetype("Roboto-Black.ttf",250)
+# 	d1.text((160,110),str(jno),font=dfont,fill=(255,255,255))
+# 	bordern=border.resize((450,450))
+# 	fieldn.paste(bordern,(580+(j*wforeach),int(h)))
+# 	j+=1
+# 	fieldn.save("how.jpg")
+
+# }
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------------------------------	
+
+# {
+# #draw and paste init
 # d1=ImageDraw.Draw(border)
 # dfont=ImageFont.truetype("Roboto-Black.ttf",250)
 
